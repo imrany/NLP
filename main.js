273cab592de58525e721e6d95b62c6a99e6f4956
imranyy <imranmatano6@gmail.com>
2023-01-03 10:34:50 +0300

    filter using for loop 2

--- main.js
+++ main.js
@@ -13,22 +13,36 @@ const companies=[
 
 const age=[44,43,45,76,98,23,7877,22,89,23,86,12,87,29,7,57,88,32,21,66,97,32,2,3,6,67,7,9]
 
-//for loop
+//forEach using for loop
 // for(let i=0; i < companies.length; i++) {
 //     console.log(companies[i])
 // }
 
-//forEach
+//forEach array method
 // companies.forEach((company,index,array) => {
 //     console.log(company)
 // });
 
 //filter using for loop
+//1 for(let i=0; i<age.length;i++) {
+//     if(age[i]>21){
+//         console.log(age[i],"Older")
+//     }
+// }
+
+const canDrink=[];
 for(let i=0; i<age.length;i++) {
-    if(age[i]>21){
-        console.log(age[i],"Older")
+    if(age[i]>=21){
+        canDrink.push(age[i])
     }
 }
+console.log(canDrink);
+
+//filter array method
+age.filter((age)=>{
+    // console.log(age)
+})
+
 //sort
 //map
 //reduce
