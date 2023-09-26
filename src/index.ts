9a565ead8c18a4b1b278644be54dd1ef463de70d
imrany <imranmat254@gmail.com>
2023-09-26 14:29:40 +0300

    new setup

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,9 @@ dotenv.config()
 
 const app=express()
 
+app.use(cors({}))
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
-app.use(cors({}))
 
 const port=process.env.PORT||8000
 app.listen(port,()=>{
